ae40af648fa048940693f1e074d3823e85727b33
BlackHyrax <72665814+BlackHyrax@users.noreply.github.com>
2023-03-09 18:32:14 +0100

    Initial Commit

---
+++ DiscordRPC.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace Discord_RPC_WPF
+{
+    partial class DiscordRPC
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.ClientID_TexBox = new System.Windows.Forms.TextBox();
+            this.State_TextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Azonix", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(25, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(521, 32);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Monastery\'s Discord RPC";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.Indigo;
+            this.label4.Location = new System.Drawing.Point(60, 258);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(128, 37);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Client ID";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.Color.Indigo;
+            this.label5.Location = new System.Drawing.Point(60, 342);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 37);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "State";
+            // 
+            // ClientID_TexBox
+            // 
+            this.ClientID_TexBox.Location = new System.Drawing.Point(60, 298);
+            this.ClientID_TexBox.Name = "ClientID_TexBox";
+            this.ClientID_TexBox.Size = new System.Drawing.Size(403, 23);
+            this.ClientID_TexBox.TabIndex = 5;
+            // 
+            // State_TextBox
+            // 
+            this.State_TextBox.Location = new System.Drawing.Point(60, 382);
+            this.State_TextBox.Name = "State_TextBox";
+            this.State_TextBox.Size = new System.Drawing.Size(403, 23);
+            this.State_TextBox.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI Black", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(60, 144);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(191, 82);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.Font = new System.Drawing.Font("Segoe UI Black", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(272, 144);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(191, 82);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // DiscordRPC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(552, 558);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.State_TextBox);
+            this.Controls.Add(this.ClientID_TexBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.DoubleBuffered = true;
+            this.Name = "DiscordRPC";
+            this.Text = "Discord RPC";
+            this.Load += new System.EventHandler(this.DiscordRPC_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox ClientID_TexBox;
+        private System.Windows.Forms.TextBox State_TextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ DiscordRPC.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DiscordRPC;
+using DiscordRPC.Logging;
+
+namespace Discord_RPC_WPF
+{
+    public partial class DiscordRPC : Form
+    {
+        public DiscordRPC()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DiscordRPC_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public DiscordRpcClient client;
+        
+        void Initialze()
+        {
+			/*
+	Create a Discord client
+	NOTE: 	If you are using Unity3D, you must use the full constructor and define
+			 the pipe connection.
+	*/
+			client = new DiscordRpcClient(ClientID_TexBox.Text);
+
+			//Set the logger
+			client.Logger = new ConsoleLogger() { Level = LogLevel.Warning };
+
+			//Subscribe to events
+			client.OnReady += (sender, e) =>
+			{
+				Console.WriteLine("Received Ready from user {0}", e.User.Username);
+			};
+
+			client.OnPresenceUpdate += (sender, e) =>
+			{
+				Console.WriteLine("Received Update! {0}", e.Presence);
+			};
+
+			//Connect to the RPC
+			client.Initialize();
+
+			//Set the rich presence
+			//Call this as many times as you want and anywhere in your code.
+			client.SetPresence(new RichPresence()
+			{
+				//Details = "Example Project",
+				State = State_TextBox.Text,
+				Assets = new Assets()
+				{
+					LargeImageKey = "mymaya",
+					//LargeImageText = "Lachee's Discord IPC Library",
+					SmallImageKey = "bunny"
+				}
+			});
+		}
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+			Initialze();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+			client.Dispose();
+        }
+    }
+}
